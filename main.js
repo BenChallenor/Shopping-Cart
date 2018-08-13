45a9d7c3a251cb668a504016620c02d4b0cdccc9
Ben <ben.challenor@btopenworld.com>
2018-08-13 17:04:57 +0100

    Able to add products to cart HTML

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 // shows more errors in the console
-
+const cartDom = document.querySelector('.cart');
 const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]');
 // selects all the add to cart buttons
 
@@ -12,9 +12,19 @@ addToCartButtonsDOM.forEach(addToCartButtonDOM => {
     // accesses the product element
     const product = {
       name: productDOM.querySelector('.product__name').innerText,
+      // innerText gets name from HTML
       price: productDOM.querySelector('.product__price').innerText
     }
     // creates product object
-    console.table(product);
+    // console.table(product);
+    cartDom.insertAdjacentHTML('beforeend', `
+    <div class="cart__item">
+      <h3 class="class__item__name">${product.name}</h3>
+      <h3 class="class__item__price"${product.price}</h3>
+      </div>
+    `);
+    // beforeend adds to the top of the cart
+    // backticks create a template string
+    // adds products to cart HTML
   });
 });
